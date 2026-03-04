2395777961775b0fab66d428bcbd7a6795be2c3d
ingimarErling <57674739+ingimarErling@users.noreply.github.com>
2026-03-04 22:08:06 +0100

    Update course.js

--- game/js/course.js
+++ game/js/course.js
@@ -1,18 +1,25 @@
 export default class Course {
 
-    constructor() {
+    constructor(){
 
         this.fairway = {
-            x: 100,
-            y: 250,
-            width: 700,
-            height: 100
+
+            x:100,
+            y:250,
+            width:700,
+            height:100
         };
 
         this.hole = {
-            x: 750,
-            y: 300,
-            radius: 8
+
+            x:750,
+            y:300,
+
+            /*
+             större hål → lättare att träffa
+            */
+
+            radius:14
         };
     }
 }
