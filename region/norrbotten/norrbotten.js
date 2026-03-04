95940622a341e150c1ae66203fb609d532af85c6
ingimarErling <57674739+ingimarErling@users.noreply.github.com>
2026-03-04 21:14:12 +0100

    Update norrbotten.js

--- region/norrbotten/norrbotten.js
+++ region/norrbotten/norrbotten.js
@@ -1,1 +1,135 @@
+// ===============================
+// CONFIG
+// ===============================
 
+const REGION_CENTER = [66.0,22.0];
+const REGION_ZOOM = 6;
+
+const GEOJSON_PATH = "../../geojson/norrbotten.geojson";
+
+
+// ===============================
+// INIT MAP
+// ===============================
+
+const map = L.map('regionMap').setView(REGION_CENTER, REGION_ZOOM);
+
+
+// ===============================
+// BACKGROUND MAP
+// ===============================
+
+L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+attribution: '&copy; OpenStreetMap contributors'
+}
+).addTo(map);
+
+
+// ===============================
+// ICONS
+// ===============================
+
+const icon9 = L.icon({
+iconUrl:"https://maps.google.com/mapfiles/ms/icons/green-dot.png",
+iconSize:[32,32]
+});
+
+const icon18 = L.icon({
+iconUrl:"https://maps.google.com/mapfiles/ms/icons/red-dot.png",
+iconSize:[32,32]
+});
+
+
+// ===============================
+// LOAD GEOJSON
+// ===============================
+
+fetch(GEOJSON_PATH)
+
+.then(response => response.json())
+
+.then(data => {
+
+const geoLayer = L.geoJSON(data,{
+
+pointToLayer:function(feature,latlng){
+
+const p = feature.properties;
+
+const holes = p.holes || 9;
+
+const icon = holes == 18 ? icon18 : icon9;
+
+const marker = L.marker(latlng,{icon});
+
+marker.bindPopup(
+
+"<strong>"+p.name+"</strong><br>"+
+
+(p.municipality || "")+"<br>"+
+
+"⛳ "+holes+" hål<br>"+
+
+(p.website
+? '<a href="'+p.website+'" target="_blank">Besök hemsida</a>'
+: "")
+
+);
+
+marker.bindTooltip(p.name);
+
+return marker;
+
+}
+
+}).addTo(map);
+
+
+// ===============================
+// AUTO ZOOM
+// ===============================
+
+map.fitBounds(geoLayer.getBounds());
+
+
+// ===============================
+// BUILD TABLE
+// ===============================
+
+const table = document.getElementById("clubTable");
+
+data.features.forEach(feature => {
+
+const p = feature.properties;
+
+table.innerHTML +=
+
+"<tr>"+
+
+"<td>"+p.name+"</td>"+
+
+"<td>"+(p.municipality || "")+"</td>"+
+
+"<td>"+(p.holes || "?")+"</td>"+
+
+"<td>"+
+
+(p.website
+? '<a href="'+p.website+'" target="_blank">Besök</a>'
+: "-")+
+
+"</td>"+
+
+"</tr>";
+
+});
+
+})
+
+.catch(err => {
+
+console.error("Kunde inte ladda geojson:",err);
+
+});
