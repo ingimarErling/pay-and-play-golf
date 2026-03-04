b51918b4e23d2ddbdd46d24f51276adc61e81d45
ingimarErling <57674739+ingimarErling@users.noreply.github.com>
2026-03-04 19:51:20 +0100

    Create course.js

---
+++ game/course.js
@@ -0,0 +1,18 @@
+export default class Course {
+
+    constructor() {
+
+        this.fairway = {
+            x: 100,
+            y: 250,
+            width: 700,
+            height: 100
+        };
+
+        this.hole = {
+            x: 750,
+            y: 300,
+            radius: 8
+        };
+    }
+}
