b64e41e562202c81610baaf64ef4b5e879cb1b8f
ingimarErling <57674739+ingimarErling@users.noreply.github.com>
2026-03-04 20:53:40 +0100

    Update ball.js

--- game/js/ball.js
+++ game/js/ball.js
@@ -11,12 +11,15 @@ export default class Ball {
         this.friction = 0.98;
     }
 
-    hit(power, angle) {
+    hit(power, angle, maxDistance) {
 
         const speed = power * 0.15;
 
         this.vx = Math.cos(angle) * speed;
         this.vy = Math.sin(angle) * speed;
+
+        this.maxDistance = maxDistance;
+        this.travel = 0;
     }
 
     update() {
@@ -24,11 +27,19 @@ export default class Ball {
         this.x += this.vx;
         this.y += this.vy;
 
+        this.travel += Math.sqrt(this.vx*this.vx + this.vy*this.vy);
+
+        if(this.travel > this.maxDistance){
+
+            this.vx = 0;
+            this.vy = 0;
+        }
+
         this.vx *= this.friction;
         this.vy *= this.friction;
     }
 
-    isStopped() {
+    isStopped(){
 
         return Math.abs(this.vx) < 0.05 && Math.abs(this.vy) < 0.05;
     }
