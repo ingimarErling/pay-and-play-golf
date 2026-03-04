dda04b061681a922ed8ec357bcee13fd1e7e3a68
ingimarErling <57674739+ingimarErling@users.noreply.github.com>
2026-03-04 20:11:51 +0100

    Update swing.js

--- game/js/swing.js
+++ game/js/swing.js
@@ -1,1 +1,29 @@
+export default class Swing {
 
+    constructor() {
+        this.power = 0;
+        this.direction = 1;
+        this.active = false;
+    }
+
+    start() {
+        this.active = true;
+        this.power = 0;
+    }
+
+    stop() {
+        this.active = false;
+        return this.power;
+    }
+
+    update(delta) {
+
+        if (!this.active) return;
+
+        this.power += this.direction * 0.4;
+
+        if (this.power > 100 || this.power < 0) {
+            this.direction *= -1;
+        }
+    }
+}
